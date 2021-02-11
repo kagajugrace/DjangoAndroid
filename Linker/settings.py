8a4bdf73aa0018610a534b2fb2c7e5ee22af6720
KAGAJU <kagajugrace@gmail.com>
2021-02-11 16:41:06 +0200

    dsc

--- Linker/settings.py
+++ Linker/settings.py
@@ -123,17 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = '/home/Desktop/AndroidDjango/Linker/static'
+# STATIC_ROOT = '/home/Desktop/AndroidDjango/Linker/static'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_URL = 'static/'
 # STATIC_ROOT=os.path.join(BASE_DIR,'assets')
@@ -143,3 +144,12 @@ STATIC_ROOT = '/home/Desktop/AndroidDjango/Linker/static'
 
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
