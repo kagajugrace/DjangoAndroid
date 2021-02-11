fa89fdce7fa2e19d2e7847ad4d7be933b87ada12
KAGAJU <kagajugrace@gmail.com>
2021-02-11 12:57:39 +0200

    djkcnslaED

--- Linker/settings.py
+++ Linker/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
